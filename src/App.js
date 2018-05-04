784c156ae99eb49b8dbc69126f9a28ac9fa00e9f
Jim Brashear <returntosender@earthlink.net>
2018-05-04 15:46:50 -0700

    go forwith

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">MONGO IS THE BONGO</h1>
+        </header>
+        <p className="App-intro">
+        <h1>Virginia is for lovers. So is Mongo. Let's bongo.</h1>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
